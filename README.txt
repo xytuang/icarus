1cbe530fef58025aa04d0a246ff55ade196813c1
Xiang Yu Tuang <xiangyutuang@gmail.com>
2024-05-20 15:38:04 -0400

    worked on different classes of expressions. (binary, grouping, literal, unary etc.) classes were generated using metaprogramming

--- README.txt
+++
@@ -1,7 +0,0 @@
-18 May 2024:
-Set up
-
-
-19 May 2024:
-Added tokens (numbers, letters, operators like less than, equal etc.) Currently
-working on how to parse different tokens.
--- token.cpp
+++
@@ -1,8 +0,0 @@
-#include "token.h"
-
-Token::Token(TokenType type, std::string lexeme, std::string literal, int line) {
-    this->type = type;
-    this->lexeme = lexeme;
-    this->literal = literal;
-    this->line = line;
-}
